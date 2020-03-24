5d12ad29ac4f99bc7a7bcee259073893ac722c09
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-24 23:45:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  animals << "Fish"
+  animals << element
 end
 
 def add_element_to_start_of_array(array, element)
-  animals.unshift("Turtle")
+  animals.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
